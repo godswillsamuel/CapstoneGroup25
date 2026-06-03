7a79360bb62585b8dbf9d28b76cd81c0d9ed83eb
Nurudeen Abdulqudus <abdulqudusnurudeen@MacBook-Pro.local>
2026-06-03 15:30:45 -0400

    add videoSection and highlight styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
-
-
+import VideoSection from "./components/videoSection";
 function App() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <VideoSection />
     </div>
   );
 }
---
+++ src/components/VideoSection.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import video from "../assets/planet:video.mp4";
+import "./VideoSection.css";
+
+const VideoSection = () => {
+  return (
+    <div className="video-section">
+      <div className="video">
+        <img src={video} alt="planetary data video thumbnail" />
+      </div>
+      <div className="text">
+        <h3>How Planetary Data Helps Us Understand Space</h3>
+        <p>
+        Planetary science goes beyond images. Comparing <span className="highlight">mass</span>, <span className="highlight">diameter</span>,
+<span className="highlight">gravity</span>, and <span className="highlight">density</span>, we gain insight into how planets form, behave,
+and interact within the solar system.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default VideoSection;
