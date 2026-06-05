9b6472af1c8e3c06b19a9c75c2423dd9ac0979d4
Nurudeen Abdulqudus <abdulqudusnurudeen@MacBook-Pro.local>
2026-06-05 15:57:08 -0400

    Fix video display and CSS

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -6,14 +6,16 @@ const VideoSection = () => {
   return (
     <div className="video-section">
       <div className="video">
-        <img src={video} alt="planetary data video thumbnail" />
+        <video autoPlay muted loop playsInline>
+          <source src={video} type="video/mp4" />
+        </video>
       </div>
       <div className="text">
         <h3>How Planetary Data Helps Us Understand Space</h3>
         <p>
-        Planetary science goes beyond images. Comparing <span className="highlight">mass</span>, <span className="highlight">diameter</span>,
-<span className="highlight">gravity</span>, and <span className="highlight">density</span>, we gain insight into how planets form, behave,
-and interact within the solar system.
+          Planetary science goes beyond images. Comparing <span className="highlight">mass</span>, <span className="highlight">diameter</span>,
+          <span className="highlight">gravity</span>, and <span className="highlight">density</span>, we gain insight into how planets form, behave,
+          and interact within the solar system.
         </p>
       </div>
     </div>
