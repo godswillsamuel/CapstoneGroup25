f291cc97bbcf8769764b524b896c6addbe442ac9
godswillsamuel <104204553+godswillsamuel@users.noreply.github.com>
2026-06-06 01:40:09 +0100

    Remove Form and Footer from App component
    Removed Form and Footer components from App.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import HeroSection from "./components/HeroSection";
 import VideoSection from "./components/VideoSection";
 import Planet from "./components/Planet";
 import Table from "./components/Table";
-import Form from "./components/form";
-import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,8 +12,6 @@ function App() {
       <VideoSection />
       <Planet />
       <Table />
-      <Form />
-      <Footer />
     </div>
   );
 }
