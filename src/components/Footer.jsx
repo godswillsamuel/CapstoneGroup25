0373387c12772fee92e093c65d30996d3ef64a4a
godswillsamuel <104204553+godswillsamuel@users.noreply.github.com>
2026-06-06 11:27:57 +0100

    Update team members in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
           <div className="footer-about">
             <h4>We are a team passionate about planetary science and data analysis.</h4>
             <p style={{ fontSize: "18px", color: "#cec8c8", marginTop: "10px" }}>
-              <strong>Team:</strong> Godswill, Ayobami, Muhammad, francis, isreal
+              <strong>Team:</strong> Godswill, Ayobami, Qudus.
             </p>
           </div>
                 <div style={{ borderTop: "1px solid rgba(255, 255, 255, 0.15)", margin: "5px " }}></div>
